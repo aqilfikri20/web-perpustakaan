0a05971e7687f99060c589fd75a3a0f3361a5d01
Mhd. Aqil Al Fikri <163704780+aqilfikri20@users.noreply.github.com>
2025-10-21 14:04:16 +0700

    perpustakaan aqil fix
    Log the API URL to the console for debugging.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,7 @@ import BeritaComponent from "../components/BeritaComponent/BeritaComponent";
 
 
 export default async function Home() {
+  console.log("API URL:", process.env.NEXT_PUBLIC_APIURL);
   const APIURL = process.env.NEXT_PUBLIC_APIURL;
   let response = await fetch(`${APIURL}/api/posts?populate=*&sort=`)
   let {data: posts} = await response.json()
